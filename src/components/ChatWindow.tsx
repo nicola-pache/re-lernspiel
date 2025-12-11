22047d5442cd7de2cc6220357093780da5bb311b
Nicola <nicola.pache@studmail.w-hs.de>
2025-12-11 17:35:37 +0100

    Chat fix

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -21,7 +21,6 @@ export default function ChatWindow({ messages, isTyping }: ChatWindowProps) {
         <AnimatePresence>
           {messages.map((msg) => (
             <motion.div
-              key={msg.id}
               initial={{ opacity: 0, x: msg.sender === "player" ? 50 : -50 }}
               animate={{ opacity: 1, x: 0 }}
               exit={{ opacity: 0 }}
